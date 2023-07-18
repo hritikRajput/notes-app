7013e1006339252895021a05c9c8339a0b4dc5e5
Ritik Rajput <hritikshivam@gmail.com>
2023-07-18 22:14:10 +0530

    Add NewNote component and append it in Home page

---
+++ src/components/NewNote/NewNote.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import "./NewNote.css"
+import plus from "../../assets/plus-solid.svg"
+export default function NewNote() {
+    const [title, setTitle] = React.useState("")
+    const [description, setDescription] = React.useState("")
+
+    const handleTitleChange = (e) => {
+        console.log(e.target)
+        setTitle(e.target.value);
+    }
+    const handleDescriptionChange = (e) => {
+        console.log(e.target)
+        setDescription(e.target.value)
+    }
+
+    const handleClick = () => {
+        console.log("Plus sign clicked")
+    }
+
+
+    return (
+        <div className="newnote">
+            <input className="newnote__title" onChange={handleTitleChange} type="text" name="title" value={title} placeholder="Add the title" />
+            <textarea className="newnote__description" onChange={handleDescriptionChange} name="description" value={description} cols="80" rows="5" placeholder="Add your note here ..." />
+            <img src={plus} className="newnote__plus" onClick={handleClick} />
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import NewNote from "../../components/NewNote/NewNote";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <NewNote />
     </div>
   );
 }
