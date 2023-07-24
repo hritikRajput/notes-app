d37732ae98c524df5ecd9d4612377184cdc0ecbb
Ritik Rajput <hritikshivam@gmail.com>
2023-07-24 19:25:17 +0530

    Add NoteCard.css and added NoteCard in Home.jsx

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -2,20 +2,21 @@ import archive from "../../assets/archive.svg"
 import pinned from "../../assets/pinned.svg"
 import unpinned from "../../assets/unpinned.svg"
 import remove from "../../assets/trash.svg"
+import "./NoteCard.css"
 
 export default function NoteCard() {
     return (
-        <div>
-            <div>
+        <div className="notecard">
+            <div className="notecard__title-row">
                 <h2>Note 1 title</h2>
-                <span><img src={unpinned} /></span>
+                <span className="notecard__img"><img src={unpinned} /></span>
             </div>
-            <div>
+            <div className="notecard__description-row">
                 <p>I am description for Note 1..........</p>
             </div>
-            <div>
-                <span><img src={archive} /></span>
-                <span><img src={remove} /></span>
+            <div className="notecard__options-row">
+                <span className="notecard__img"><img src={archive} /></span>
+                <span className="notecard__img"><img src={remove} /></span>
             </div>
         </div>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import NewNote from "../../components/NewNote/NewNote";
+import NoteCard from "../../components/NoteCard/NoteCard";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <NewNote />
+      <NoteCard />
     </div>
   );
 }
