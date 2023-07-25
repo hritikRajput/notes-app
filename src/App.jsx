b4659eb12c3a04d1647ac8d3f05cd53d6f8dd306
Ritik Rajput <hritikshivam@gmail.com>
2023-07-26 01:03:36 +0530

    Add react-router-dom, and create an Archive page, and use react-router-dom in app.js for setting up route for Home and Archive page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Home from "./pages/Home/Home"
+import Archive from "./pages/Archive/Archive"
 import './App.css'
 import { NotesProvider } from "./context/notesContext"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <NotesProvider>
-      <div>
-        <Home />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/archive" element={<Archive />} />
+      </Routes>
     </NotesProvider>
 
   )
---
+++ src/pages/Archive/Archive.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+
+export default function Archive() {
+    return (
+        <div>
+            <Header />
+            <Sidebar />
+            <h2>Archive Notes</h2>
+        </div>
+    );
+}
