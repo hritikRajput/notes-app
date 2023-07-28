748b472d72e0f3d6ea0e4a8c54e9d154c02ed858
Ritik Rajput <hritikshivam@gmail.com>
2023-07-28 14:50:40 +0530

    Add style in bin.css

--- src/pages/Bin/Bin.jsx
+++ src/pages/Bin/Bin.jsx
@@ -2,18 +2,21 @@ import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import NoteCard from "../../components/NoteCard/NoteCard";
 import { useNotes } from "../../context/notesContext"
+import "./Bin.css"
 
 export default function Bin() {
     const { deletedNotes } = useNotes()
     return (
-        <div>
-            <Header />
-            <Sidebar />
-            <div>
-                {deletedNotes.length > 0 && <h2>Deleted Notes</h2>}
-                {
-                    deletedNotes.map(note => <NoteCard key={note.id} title={note.title} description={note.description} id={note.id} />)
-                }
+        <div className="bin__grid-container">
+            <Header className="header" />
+            <Sidebar className="sidebar" />
+            <div className="bin__notecard-container">
+                {deletedNotes.length > 0 ? <h2>Deleted Notes</h2> : <p className="no-notes">There is nothing to show</p>}
+                <div className="bin__notecard">
+                    {
+                        deletedNotes.map(note => <NoteCard key={note.id} title={note.title} description={note.description} id={note.id} />)
+                    }
+                </div>
             </div>
         </div>
     );
