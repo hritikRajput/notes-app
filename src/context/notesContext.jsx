712de261b114a3f8a4cf2a61844a1042a2e1e34c
Ritik Rajput <hritikshivam@gmail.com>
2023-08-15 16:20:37 +0530

    Add local storage in notesContext.jsx to store data

--- src/context/notesContext.jsx
+++ src/context/notesContext.jsx
@@ -1,15 +1,37 @@
-import React, { useContext, createContext } from "react";
+import React, { useContext, useEffect, createContext } from "react";
 
 const NotesContext = createContext();
 
 const NotesProvider = (props) => {
   const [title, setTitle] = React.useState("");
   const [description, setDescription] = React.useState("");
-  const [notes, setNotes] = React.useState([]);
-  const [pinnedNotes, setPinnedNotes] = React.useState([]);
-  const [archiveNotes, setArchiveNotes] = React.useState([]);
-  const [importantNotes, setImportantNotes] = React.useState([]);
-  const [deletedNotes, setDeletedNotes] = React.useState([]);
+  const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem('notes')) || []);
+  const [pinnedNotes, setPinnedNotes] = React.useState(JSON.parse(localStorage.getItem('pinnedNotes')) || []);
+  const [archiveNotes, setArchiveNotes] = React.useState(JSON.parse(localStorage.getItem('archiveNotes')) || []);
+  const [importantNotes, setImportantNotes] = React.useState(JSON.parse(localStorage.getItem('importantNotes')) || []);
+  const [deletedNotes, setDeletedNotes] = React.useState(JSON.parse(localStorage.getItem('deletedNotes')) || []);
+
+  // Update localStorage whenever the relevant state changes
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    localStorage.setItem('pinnedNotes', JSON.stringify(pinnedNotes));
+  }, [pinnedNotes]);
+
+  useEffect(() => {
+    localStorage.setItem('archiveNotes', JSON.stringify(archiveNotes));
+  }, [archiveNotes]);
+
+  useEffect(() => {
+    localStorage.setItem('importantNotes', JSON.stringify(importantNotes));
+  }, [importantNotes]);
+
+  useEffect(() => {
+    localStorage.setItem('deletedNotes', JSON.stringify(deletedNotes));
+  }, [deletedNotes]);
+
 
   return (
     <NotesContext.Provider
