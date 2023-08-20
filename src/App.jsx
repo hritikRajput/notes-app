d56180b15cc7f2ba4e675b846f3fbe49c9a0aae6
Ritik Rajput <hritikshivam@gmail.com>
2023-08-21 03:02:31 +0530

    Add NoteProvider in main.jsx and remove from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import Archive from "./pages/Archive/Archive"
 import Important from "./pages/Important/Important"
 import Bin from "./pages/Bin/Bin"
 import './App.css'
-import { NotesProvider } from "./context/notesContext"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <NotesProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/important" element={<Important />} />
-        <Route path="/bin" element={<Bin />} />
-      </Routes>
-    </NotesProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/important" element={<Important />} />
+      <Route path="/bin" element={<Bin />} />
+    </Routes>
 
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 import { BrowserRouter } from "react-router-dom"
+import { NotesProvider } from './context/notesContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <NotesProvider>
+        <App />
+      </NotesProvider>
     </React.StrictMode>
   </BrowserRouter>
 )
